4615eaad85eef5cd75ffe8e51a136eaa6d18e37f
Dario Vladovic <d.vladimyr@gmail.com>
2019-05-21 01:43:29 +0200

    Cleanup :lipstick:
    - enabling pm2 configuration through `pm2` field inside
      `package.json`

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const { format } = require('util');
+const { name } = require('./package.json');
+const { pkg } = require('read-pkg-up').sync();
 const dotenv = require('dotenv');
 const kleur = require('kleur');
 const path = require('path');
-const pkg = require('read-pkg-up').sync();
 
-const PREFIX = '[PM2]';
+const colorMap = createColorLookup(kleur);
 const rePM2ConfigVar = /^PM2_CONFIG_/i;
 
-const logInfo = (msg, ...args) => console.error(kleur.green(PREFIX), format(msg, ...args));
-const logError = (msg, ...args) => console.error(kleur.red(`${PREFIX}[ERROR]`), format(msg, ...args));
+const logInfo = (msg, ...args) => log(kleur.cyan, msg, ...args);
+const logError = (msg, ...args) => log(kleur.red, msg, ...args);
 
 logInfo('Reading %s', path.resolve(process.cwd(), '.env'));
 const { error } = dotenv.config();
@@ -21,16 +22,29 @@ if (error) {
 }
 
 module.exports = (options = {}) => {
-  const config = readAppConfig();
-  const startOptions = Object.assign({ name: pkg.name }, config, options);
-  return [startOptions];
+  const envConfig = readEnvConfig();
+  const config = Object.assign({ name: pkg.name }, pkg.pm2, envConfig, options);
+  return [config];
 };
 
-function readAppConfig(env = process.env) {
+function readEnvConfig(env = process.env) {
   return Object.entries(env).reduce((config, [key, value]) => {
     if (!rePM2ConfigVar.test(key)) return config;
     key = key.toLowerCase().replace(rePM2ConfigVar, '');
     if (key.startsWith('env')) return config;
     return Object.assign(config, { [key]: value });
   }, {});
+}
+
+function log(colorFn, msg, ...args) {
+  const color = colorMap.get(colorFn);
+  const prefix = kleur.inverse()[color]().bold(` ${name} `);
+  return console.error(prefix, format(msg, ...args));
+}
+
+function createColorLookup(colors) {
+  return Object.entries(colors).reduce((acc, [color, colorFn]) => {
+    acc.set(colorFn, color);
+    return acc;
+  }, new Map());
 }
